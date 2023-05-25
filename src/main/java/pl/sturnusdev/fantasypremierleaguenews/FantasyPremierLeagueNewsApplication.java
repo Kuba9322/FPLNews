8a96c4f8cda9b6fa1188b96c818e072fa5ff0bd0
Kkuba9322 <kkuba9322@gmail.com>
2023-05-25 22:28:23 +0200

    1 - 250523 - Add first page and css stylesheet.

---
+++ src/main/java/pl/sturnusdev/fantasypremierleaguenews/FantasyPremierLeagueNewsApplication.java
@@ -0,0 +1,13 @@
+package pl.sturnusdev.fantasypremierleaguenews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyPremierLeagueNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FantasyPremierLeagueNewsApplication.class, args);
+    }
+
+}
